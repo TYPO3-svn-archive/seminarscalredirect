a14ba8fae1f65e1102a11c06f3222a974a0da95e
oliklee <oliklee@735d13b6-9817-0410-8766-e36946ffe9aa>
2010-05-21 09:29:13 +0000

    some cleanup
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/seminarscalredirect/trunk@33358 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext "seminarscalredirect".
 #
-# Auto generated 21-05-2010 11:19
+# Auto generated 21-05-2010 11:28
 #
 # Manual updates:
 # Only the data in the array - everything else is removed by next
@@ -28,22 +28,26 @@ $EM_CONF[$_EXTKEY] = array(
 	'modify_tables' => '',
 	'clearCacheOnLoad' => 0,
 	'lockType' => '',
-	'author_company' => '',
-	'version' => '0.0.0',
+	'author_company' => 'oliverklee.de',
+	'version' => '0.1.0',
 	'constraints' => array(
 		'depends' => array(
+			'php' => '5.2.0-0.0.0',
+			'typo3' => '4.3.0-0.0.0',
 			'cal' => '',
 			'cal_ts_service' => '',
-			'oelib' => '',
-			'onetimeaccount' => '',
-			'seminars' => '',
+			'oelib' => '0.7.0-',
+			'onetimeaccount' => '0.7.0-',
+			'seminars' => '0.9.0-',
 		),
 		'conflicts' => array(
 		),
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:9:{s:9:"ChangeLog";s:4:"ac59";s:12:"ext_icon.gif";s:4:"1bdc";s:17:"ext_localconf.php";s:4:"6cde";s:14:"ext_tables.php";s:4:"885a";s:16:"locallang_db.xml";s:4:"ae32";s:19:"doc/wizard_form.dat";s:4:"b18a";s:20:"doc/wizard_form.html";s:4:"7736";s:40:"pi1/class.tx_seminarscalredirect_pi1.php";s:4:"179f";s:17:"pi1/locallang.xml";s:4:"54f9";}',
+	'_md5_values_when_last_written' => 'a:7:{s:9:"ChangeLog";s:4:"ac59";s:16:"ext_autoload.php";s:4:"1cbb";s:12:"ext_icon.gif";s:4:"1bdc";s:17:"ext_localconf.php";s:4:"6cde";s:14:"ext_tables.php";s:4:"885a";s:16:"locallang_db.xml";s:4:"6519";s:40:"pi1/class.tx_seminarscalredirect_pi1.php";s:4:"cc4b";}',
+	'suggests' => array(
+	),
 );
 
 ?>
