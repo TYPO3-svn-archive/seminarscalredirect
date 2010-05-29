4613975b86c7e8346a6a697634dd711a2b5e42a9
oliklee <oliklee@735d13b6-9817-0410-8766-e36946ffe9aa>
2010-05-29 10:06:24 +0000

    Release seminarscalredirect 1.0.0.
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/seminarscalredirect/trunk@33681 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext "seminarscalredirect".
 #
-# Auto generated 21-05-2010 11:55
+# Auto generated 29-05-2010 12:05
 #
 # Manual updates:
 # Only the data in the array - everything else is removed by next
@@ -29,7 +29,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'clearCacheOnLoad' => 0,
 	'lockType' => '',
 	'author_company' => 'oliverklee.de',
-	'version' => '0.1.0',
+	'version' => '1.0.0',
 	'constraints' => array(
 		'depends' => array(
 			'php' => '5.2.0-0.0.0',
@@ -45,7 +45,7 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:7:{s:9:"ChangeLog";s:4:"ac59";s:16:"ext_autoload.php";s:4:"1cbb";s:12:"ext_icon.gif";s:4:"1bdc";s:17:"ext_localconf.php";s:4:"6cde";s:14:"ext_tables.php";s:4:"885a";s:16:"locallang_db.xml";s:4:"6519";s:40:"pi1/class.tx_seminarscalredirect_pi1.php";s:4:"905e";}',
+	'_md5_values_when_last_written' => 'a:8:{s:9:"ChangeLog";s:4:"1921";s:16:"ext_autoload.php";s:4:"1cbb";s:12:"ext_icon.gif";s:4:"1bdc";s:17:"ext_localconf.php";s:4:"6cde";s:14:"ext_tables.php";s:4:"885a";s:16:"locallang_db.xml";s:4:"6519";s:14:"doc/manual.sxw";s:4:"5355";s:40:"pi1/class.tx_seminarscalredirect_pi1.php";s:4:"905e";}',
 	'suggests' => array(
 	),
 );
